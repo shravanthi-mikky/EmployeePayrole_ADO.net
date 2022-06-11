c614191e3216a83a976111f822eb75c46c6d9fa8
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-11 14:08:00 +0530

    [shravanthi] -Creating database and Insert values through VS

---
+++ EmployeePayrollService_ADO.net/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeePayrollService_ADO.net;
+
+Console.WriteLine("Welcome To Employee_Payroll ADO.NET !");
+
+EmployeeRepo employeeRepo = new EmployeeRepo();
+employeeRepo.GetEmployeedetails();
