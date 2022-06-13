c738d3c2f657c6844a4c83663d806f0d538a81ff
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-13 09:35:14 +0530

    [shravanthi] -Update salary to 3000000 of an Employee and TC

--- EmployeePayrollService_ADO.net/Program.cs
+++ EmployeePayrollService_ADO.net/Program.cs
@@ -1,7 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeePayrollService_ADO.net;
+using System.Configuration;
+using System.Data.SqlClient;
 
 Console.WriteLine("Welcome To Employee_Payroll ADO.NET !");
 
 EmployeeRepo employeeRepo = new EmployeeRepo();
-employeeRepo.GetEmployeedetails();
+Employee_details employeeDetails = new Employee_details();
+
+
+while (true)
+{
+    Console.WriteLine("Choose the option :\n1)Create and retrieve values from Database\n2)Update salary");
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            employeeRepo.GetEmployeedetails();
+            break;
+        case 2:
+            employeeRepo.updateSalary();
+            int salary = employeeRepo.updateSalary();
+            Console.WriteLine(salary);
+            employeeRepo.GetEmployeedetails();
+            break;
+        default:
+            Console.WriteLine("Please choose the correct option");
+            break;
+    }
+}
+
+
---
+++ EmployeePayrollService_ADO.netTests/EmployeeRepoTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeePayrollService_ADO.net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService_ADO.net.Tests
+{
+    [TestClass()]
+    public class EmployeeRepoTests
+    {
+        [TestMethod()]
+        public void updateSalaryTest()
+        {
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            Employee_details employeeDetails = new Employee_details();
+            int expected = 3000000;
+            employeeRepo.updateSalary();
+            int salary = employeeRepo.updateSalary();
+            Assert.AreEqual(expected, salary);
+        }
+    }
+}
